ec3579f9aa5dad85b8ab489f1995dcd12552d7d3
duxiyao <373071116@qq.com>
2015-11-12 18:00:08 +0800

    add a fun

--- KJFrame/src/org/kymjs/kjframe/utils/ImgUtil.java
+++ KJFrame/src/org/kymjs/kjframe/utils/ImgUtil.java
@@ -13,11 +13,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.NinePatchDrawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.util.Base64;
 
 public class ImgUtil {
+
+	public static Bitmap drawable2Bitmap(Drawable drawable) {
+		if (drawable instanceof BitmapDrawable) {
+			return ((BitmapDrawable) drawable).getBitmap();
+		} else if (drawable instanceof NinePatchDrawable) {
+			Bitmap bitmap = Bitmap
+					.createBitmap(
+							drawable.getIntrinsicWidth(),
+							drawable.getIntrinsicHeight(),
+							drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+									: Bitmap.Config.RGB_565);
+			Canvas canvas = new Canvas(bitmap);
+			drawable.setBounds(0, 0, drawable.getIntrinsicWidth(),
+					drawable.getIntrinsicHeight());
+			drawable.draw(canvas);
+			return bitmap;
+		} else {
+			return null;
+		}
+	}
 
 	public static Bitmap uri2Bmp(Uri uri) throws Exception {
 		ContentResolver cr = KJActivityStack.create().topActivity()
